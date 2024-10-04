f8599160bb44632aaeed087a6a07412acdc2ade2
Rohail Ramesh <rohailramesh@hotmail.com>
2024-10-04 16:13:26 +0100

    CRUD Operations

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+//main file for backend
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+//MongoDB connection first and then connect to server
+mongoose
+  .connect(
+    "mongodb+srv://rohailramesh:hBpPFwqSWHKkiqcW@cluster0.wa2db.mongodb.net/"
+  )
+  .then(() => {
+    //Logging to the console whatever is being listened to
+    app.listen(port, () => {
+      console.log("Connected to MongoDB");
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ backend/routes/product.route.js
@@ -0,0 +1,23 @@
+//all the routes for products
+const express = require("express");
+const router = express.Router();
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product.controller.js");
+
+router.get("/", getProducts);
+router.get("/:id", getProduct);
+
+router.post("/", createProduct);
+
+// update a product
+router.put("/:id", updateProduct);
+
+// delete a product
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
